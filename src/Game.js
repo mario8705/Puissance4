0083557a23f4faa7328d35cb28fa7a3a68ab82aa
Alexis Lavaud <alexislavaud191@gmail.com>
2018-09-06 13:12:19 +0200

    working p4 board

--- src/Game.js
+++ src/Game.js
@@ -4,23 +4,41 @@ import React, { Component } from 'react';
 class Game extends Component {
     state = {
         board: [
-            [ '', '', '', '', '', '', ''],
-            [ '', '', '', '', '', '', ''],
-            [ '', '', '', '', '', '', ''],
-            [ '', '', '', '', '', '', ''],
-            [ '', '', '', '', '', '', ''],
-            [ '', '', '', '', '', '', ''],
+            [ '', '', '', '', '', ''],
+            [ '', '', '', '', '', ''],
+            [ '', '', '', '', '', ''],
+            [ '', '', '', '', '', ''],
+            [ '', '', '', '', '', ''],
+            [ '', '', '', '', '', ''],
+            [ '', '', '', '', '', ''],
         ]
     }
 
     handleColumnClick = (col) => {
-        console.log(col);
+        this.setState(({ board }) => {
+            var i;
+
+            for (i = board[col].length - 1; i >= 0; --i) {
+                if (!board[col][i]) {
+                    break;
+                }
+            }
+
+            board[col][i] = 'red';
+            return { board }
+        })
     }
 
     render() {
+        const { board } = this.state;
+
         return (
             <Board>
-                <Board.Column onClick={() => this.handleColumnClick(0)} rows={[]}} />
+                {
+                    board.map((row, i) => (
+                        <Board.Column key={i} onClick={() => this.handleColumnClick(i)} rows={row} />
+                    ))
+                }
             </Board>
         );
     }
