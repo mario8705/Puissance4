770ac418adad99cd9146fb94dead115a0ef1edcd
Alexis Lavaud <alexislavaud191@gmail.com>
2018-09-11 10:44:07 +0200

    Multiplayer UI

--- src/App.js
+++ src/App.js
@@ -1,53 +1,50 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component, Fragment, Provider } from 'react';
 import { Route, Switch } from 'react-router-dom';
+import MainMenu from './MainMenu';
 import Game from './Game';
+import Multiplayer from './Multiplayer';
 import logo from './logo.png';
 import './App.css';
 
 class App extends Component {
-  componentWillMount() {
-    this.audio = new Audio();
-    this.audio.src = '/Free.mp3';
-    this.audio.autoplay = true;
-  }
+    componentWillMount() {
+        this.audio = new Audio();
+        this.audio.src = '/Free.mp3';
+        this.audio.autoplay = true;
+        this.audio.loop = true;
+    }
 
-  render() {
-    return (
-      <Fragment>
-      <div className="SpeakerOn">
-        <i
-          onClick={() => {
-            this.audio.paused ? this.audio.play() : this.audio.pause();
-            this.forceUpdate();
-          }}
-          className={'fa ' + (this.audio.paused ? 'fa-volume-off' : 'fa-volume-up')} />
-      </div>
-        <Switch>
-            <Route path="/" exact render={() => (
-                <div className="App">
-                    <header className="App-header">
-                        <img src={logo} className="App-logo" alt="logo" />
-                        <h1 className="App-title">Bienvenue sur Puissance 4</h1>
-                        <div className="TheButtons">
-                            <button className="ButtonSolo" type="button"><a href="/game">1V1</a></button>
-                            <button className="ButtonMulti" type="button"><a href="/game">Multiplayer</a></button>
-                        </div>
-                        <div className="SpeakerOn">
-                          <i
-                            onClick={() => {
-                              this.audio.paused ? this.audio.play() : this.audio.pause()
-                              this.forceUpdate()
-                            }}
-                            className={'fa ' + (this.audio.paused ? 'fa-volume-off' : 'fa-volume-up')} />
-                        </div>
-                    </header>
+    render() {
+        const val = {
+            playing: this.audio.playing,
+            setPlaying: playing => {
+                if (playing) {
+                    this.audio.pause();
+                } else {
+                    this.audio.play();
+                }
+            }
+        }
+
+        return (
+            <Fragment>
+                <div className="SpeakerOn">
+                    <i
+                        onClick={() => {
+                            this.audio.paused ? this.audio.play() : this.audio.pause();
+                            this.forceUpdate();
+                        }}
+                        className={'fa ' + (this.audio.paused ? 'fa-volume-off' : 'fa-volume-up')} />
                 </div>
-            )} />
-            <Route path="/game" exact component={Game} />
-      </Switch>
-      </Fragment>
-    );
-  }
+
+                <Switch>
+                    <Route path="/" exact component={MainMenu} />
+                    <Route path="/game" exact component={Game} />
+                    <Route path="/online" exact component={Multiplayer} />
+                </Switch>
+            </Fragment>
+        );
+    }
 }
 
 export default App;
