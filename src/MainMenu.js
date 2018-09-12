0577c0a152be663e82c5213459f02eb3ab68beef
Alexis Lavaud <alexislavaud191@gmail.com>
2018-09-12 22:47:01 +0200

    Responsive main menu

--- src/MainMenu.js
+++ src/MainMenu.js
@@ -3,16 +3,14 @@ import { Link } from 'react-router-dom';
 import logo from './logo.png';
 
 const MainMenu = () => (
-    <div className="App">
-        <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">Bienvenue sur Puissance 4</h1>
-            <div className="TheButtons">
-                <button className="ButtonSolo" type="button"><Link to="/game">1V1</Link></button>
-                <button className="ButtonMulti" type="button"><Link to="/online">Multiplayer</Link></button>
-            </div>
-        </header>
-    </div>
+    <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1 className="App-title">Bienvenue sur Puissance 4</h1>
+        <div className="TheButtons">
+            <Link to="/game">1V1</Link>
+            <Link to="/online">Multiplayer</Link>
+        </div>
+    </header>
 );
 
 export default MainMenu;
