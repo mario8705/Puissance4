2db67b53edf2ce21fffd423eab17d3be16abac42
Djo69 <jlebrun@edenschool.fr>
2018-09-12 15:23:55 +0200

    JEton

--- src/Board.js
+++ src/Board.js
@@ -1,61 +1,51 @@
 import React, { Component } from 'react';
 
 class Board extends Component {
+  constructor(props) {
+        super(props);
+        this.state = {'active': false, 'class': 'album'};
+    }
+
+  handleClick(id) {
+    if(this.state.active){
+      this.setState({'active': false,'class': 'album'})
+    }else{
+      this.setState({'active': true,'class': 'active'})
+    }
+  }
     render() {
+
         const { children } = this.props;
 
         return (
-          <div className="board">
-            <svg width="500" height="500" viewBox="0 0 510 500" xmlns="http://www.w3.org/2000/svg">
-              <path d="M10 10"/>
-              <rect width="550" height="550" style={{ fill: 'rgb(0,0,255)', 'strokeWidth': 3, 'stroke': 'rgb(0,0,0)'}} />
-                <circle cx="50" cy="60" r="27" fill="white"/>
-                <circle cx="120" cy="60" r="27" fill="white"/>
-                <circle cx="190" cy="60" r="27" fill="white"/>
-                <circle cx="260" cy="60" r="27" fill="white"/>
-                <circle cx="330" cy="60" r="27" fill="white"/>
-                <circle cx="400" cy="60" r="27" fill="white"/>
-                <circle cx="470" cy="60" r="27" fill="white"/>
-
-                <circle cx="50" cy="140" r="27" fill="white"/>
-                <circle cx="120" cy="140" r="27" fill="white"/>
-                <circle cx="190" cy="140" r="27" fill="white"/>
-                <circle cx="260" cy="140" r="27" fill="white"/>
-                <circle cx="330" cy="140" r="27" fill="white"/>
-                <circle cx="400" cy="140" r="27" fill="white"/>
-                <circle cx="470" cy="140" r="27" fill="white"/>
-
-                <circle cx="50" cy="220" r="27" fill="white"/>
-                <circle cx="120" cy="220" r="27" fill="white"/>
-                <circle cx="190" cy="220" r="27" fill="white"/>
-                <circle cx="260" cy="220" r="27" fill="white"/>
-                <circle cx="330" cy="220" r="27" fill="white"/>
-                <circle cx="400" cy="220" r="27" fill="white"/>
-                <circle cx="470" cy="220" r="27" fill="white"/>
-
-                <circle cx="50" cy="300" r="27" fill="white"/>
-                <circle cx="120" cy="300" r="27" fill="white"/>
-                <circle cx="190" cy="300" r="27" fill="white"/>
-                <circle cx="260" cy="300" r="27" fill="white"/>
-                <circle cx="330" cy="300" r="27" fill="white"/>
-                <circle cx="400" cy="300" r="27" fill="white"/>
-                <circle cx="470" cy="300" r="27" fill="white"/>
-
-                <circle cx="50" cy="370" r="27" fill="white"/>
-                <circle cx="120" cy="370" r="27" fill="white"/>
-                <circle cx="190" cy="370" r="27" fill="white"/>
-                <circle cx="260" cy="370" r="27" fill="white"/>
-                <circle cx="330" cy="370" r="27" fill="white"/>
-                <circle cx="400" cy="370" r="27" fill="white"/>
-                <circle cx="470" cy="370" r="27" fill="white"/>
-
-                <circle cx="50" cy="440" r="27" fill="white"/>
-                <circle cx="120" cy="440" r="27" fill="white"/>
-                <circle cx="190" cy="440" r="27" fill="white"/>
-                <circle cx="260" cy="440" r="27" fill="white"/>
-                <circle cx="330" cy="440" r="27" fill="white"/>
-                <circle cx="400" cy="440" r="27" fill="white"/>
-                <circle cx="470" cy="440" r="27" fill="white"/>
+          <div className="wrapper">
+            <ul className="coinContainer">
+              {
+                (new Array(6)).fill(null).map((_, y) => {
+                  return (new Array(7)).fill(null).map((_, x) => {
+                    return (
+                      <li className="coin" style={{left:(x*59) + 'px', top:(y*59) + 'px'}} onClick=""></li>                  )
+                  })
+                })
+              }
+            </ul>
+            <svg width="415" height="355" viewBox="0 0 415 355" xmlns="http://www.w3.org/2000/svg">
+              <defs>
+                <mask id="mask">
+                    <rect fill="white" width="60" height="60"/>
+                    <circle cx="30" cy="30" r="25" stroke="black" stroke-width="2" fill="black" />
+                </mask>
+                <rect id="case" style={{ mask: "url(#mask)" }} x="0" y="0" width="60" height="60" fill="blue"/>
+              </defs>
+                {
+                  (new Array(6)).fill(null).map((_, y) => {
+                    return (new Array(7)).fill(null).map((_, x) => {
+                      return (
+                        <use href="#case" x={x*59} y={y*59}/>
+                      )
+                    })
+                  })
+                }
             </svg>
           </div>
         );
